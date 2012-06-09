7cfd536dafe3599082f64d1f5e2af94cd6a6f55b
Dominick Pham <dominick.pham@gmail.com>
2012-06-09 05:42:37 +0100

    added getter

--- gpio.js
+++ gpio.js
@@ -7,26 +7,50 @@ var puts = function(err) { if(err) util.puts("error: ", err); };
 
 
 
-var GPIO = function(number) {
+var GPIO = function(number, dir) {
 	this.number = number;
 	this._n = 'gpio'+number;
 
 	this.export();
+	this.setDirection(dir);
 }
 
-GPIO.prototype._write = function(str, cmd, fn) {
+GPIO.prototype._write = function(str, file, fn) {
 	if(typeof fn === "undefined") fn = puts;
-	fs.writeFile(gpiopath + cmd, str, fn);
+	fs.writeFile(gpiopath + file, str, fn);
 };
 
-GPIO.prototype.export = function() { this._write(this.number, 'export'); };
+GPIO.prototype._read = function(file, fn) {
+	fs.readFile(gpiopath + file, "utf-8", fn || function(err, data) {
+		if(err) throw err;
+		util.puts(data);
+	});
+}; 
+
+/**
+ * Export and unexport gpio#
+ */
 GPIO.prototype.unexport = function() { this._write(this.number, 'unexport'); };
-GPIO.prototype.setDirection = function(dir) { this._write(dir, this._n + '/direction'); };
+GPIO.prototype.export = function() {
+	this._write(this.number, 'export', function(err) {
+		if(err) util.puts("error: ", err, "port possibly already exported");
+	});
+};
+
+/**
+ * Sets direction, default is "out"
+ */
+GPIO.prototype.setDirection = function(dir) {
+	if(typeof dir!== "string" || dir !== "in") dir = "out";
+	this._write(dir, this._n + '/direction');
+	this.direction = dir;
+};
+
+GPIO.prototype.get = function(fn) { this._read(this._n + '/value', fn); };
 GPIO.prototype.set = function() { this._write('1', this._n + '/value'); };
 GPIO.prototype.reset = function() { this._write('0', this._n + '/value'); };
 
 
-exports.export = function(number) { return new GPIO(number); };
+exports.export = function(number, direction) { return new GPIO(number, direction); };
 exports.unexport = function(number) { exec('echo "' + number + '" > ' + gpiopath + 'unexport', puts); };
-
 
